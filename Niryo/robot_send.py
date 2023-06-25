8403e226cad7448d8a825bbfda01eed8d79822cd
Hari Ganesan <hariganation@gmail.com>
2023-06-25 19:52:16 +1000

    Code for robot to send data

---
+++ Niryo/robot_send.py
@@ -0,0 +1,12 @@
+import pyniryo
+from azure.iot.device import IoTHubDeviceClient, Message  #version 2.5.1
+
+device_connection_string = "HostName=NiryoIOTServer.azure-devices.net;DeviceId=Niryo_Robot;SharedAccessKey=H0BXq3Dz1iEZmcBy2UUoewHO/p8R4sWcm7OOR+feMjI="
+
+device_client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
+device_client.connect()
+
+data = pyniryo.Robot.get_joints()
+message = Message(data)
+device_client.send_message(message)
+device_client.disconnect()
