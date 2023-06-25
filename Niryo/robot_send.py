a8722efdee49e85b38207d058d1f872f8bb3fe1c
Hari Ganesan <hariganation@gmail.com>
2023-06-25 19:57:11 +1000

    Corrected data type

--- Niryo/robot_send.py
+++ Niryo/robot_send.py
@@ -1,5 +1,6 @@
 import pyniryo
 from azure.iot.device import IoTHubDeviceClient, Message  #version 2.5.1
+import json
 
 device_connection_string = "HostName=NiryoIOTServer.azure-devices.net;DeviceId=Niryo_Robot;SharedAccessKey=H0BXq3Dz1iEZmcBy2UUoewHO/p8R4sWcm7OOR+feMjI="
 
@@ -7,6 +8,6 @@ device_client = IoTHubDeviceClient.create_from_connection_string(device_connecti
 device_client.connect()
 
 data = pyniryo.Robot.get_joints()
-message = Message(data)
+message = Message(json.dump(data))
 device_client.send_message(message)
 device_client.disconnect()
