54066e706a9327cdd1e9a148c4ab574d10ec8cb3
Hari Ganesan <hariganation@gmail.com>
2023-10-18 22:35:53 +0530

    Fixed connection problem

--- Niryo/robot_send.py
+++ Niryo/robot_send.py
@@ -1,13 +1,13 @@
-import pyniryo
 from azure.iot.device import IoTHubDeviceClient, Message  #version 2.5.1
 import json
 
-device_connection_string = "HostName=NiryoIOTServer.azure-devices.net;DeviceId=Niryo_Robot;SharedAccessKey=H0BXq3Dz1iEZmcBy2UUoewHO/p8R4sWcm7OOR+feMjI="
+device_connection_string = "HostName=niryoiot.azure-devices.net;DeviceId=NiryoNed2;SharedAccessKey=Ee0uclVHdESQI+NPmG8z5G3ZA1ET0dkpiZ5lbyOY4Oo=="
 
 device_client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
 device_client.connect()
 
-data = pyniryo.Robot.get_joints()
-message = Message(json.dumps(data))
+data = "0,0,0,0,0,0"
+# data = "0.0007930673506395536, 0.27671192603916284, -1.0264062213186669, -0.025985019804263043, -0.263937349106123, 0.0706557698325323"
+message = Message(data)
 device_client.send_message(message)
 device_client.disconnect()
