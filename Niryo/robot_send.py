70d38d99212b08fdfeeffcad3e0cad3023b2d3bb
Hari Ganesan <hariganation@gmail.com>
2023-10-20 10:21:51 +0530

    Changed value format

--- Niryo/robot_send.py
+++ Niryo/robot_send.py
@@ -6,8 +6,8 @@ device_connection_string = "HostName=niryoiot.azure-devices.net;DeviceId=NiryoNe
 device_client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
 device_client.connect()
 
-data = "0,0,0,0,0,0"
-# data = "0.0007930673506395536, 0.27671192603916284, -1.0264062213186669, -0.025985019804263043, -0.263937349106123, 0.0706557698325323"
+# data = "0,0,0,0,0,0,0,0,0"
+data = "0, 0, 0.0007930673506395536, 0.27671192603916284, -1.0264062213186669, -0.025985019804263043, -0.263937349106123, 0.0706557698325323, 0"
 message = Message(data)
 device_client.send_message(message)
 device_client.disconnect()
