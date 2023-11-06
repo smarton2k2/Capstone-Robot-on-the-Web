44296049926b30e801178140eec5392eeb2512e1
Hari Ganesan <hariganation@gmail.com>
2023-11-06 13:31:53 +0530

    Removed joint state temp.

--- data_collection.py
+++ data_collection.py
@@ -1,53 +1,42 @@
 #!/usr/bin/env python3
 
-#Libs
 import csv
 import rospy
-
-#Messages
 from niryo_robot_msgs.msg import HardwareStatus
 from sensor_msgs.msg import JointState
 
-# Initialize lists for data collection
-temp = []
-pos = []
-load = []
+# Open the CSV file for writing
+csv_file = open('temperature_voltage_data.csv', 'w', newline='')
+writer = csv.writer(csv_file)
 
-# Callback for hardware status
+# Write the header row based on the expected message fields
+header = ["Seq", "Time Secs", "Time Nsecs", "Temperatures", "Voltages"]
+writer.writerow(header)
+
 def callback_HS(data):
-    temp.append(data.temperature)
-    load.append(data.load)
+    # Create a list of the data fields you want to write to the CSV
+    row = [
+        data.header.seq,
+        data.header.stamp.secs,
+        data.header.stamp.nsecs,
+        ';'.join(map(str, data.temperatures)),  # Convert each temperature to string and join
+        ';'.join(map(str, data.voltages)),      # Convert each voltage to string and join
+    ]
+    
+    # Write the data to the CSV and flush to ensure it's written to disk
+    writer.writerow(row)
+    csv_file.flush()
 
-# Callback for joint state
-def callback_JS(data):
-    pos.append(data.position)
-
-# Save data to CSV
-def save_to_csv():
-    with open('data.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Temperature", "Position", "Load"])
-        # Iterate over the longest list and handle missing data
-        max_length = max(len(temp), len(pos), len(load))
-        for i in range(max_length):
-            row = [
-                temp[i] if i < len(temp) else None,
-                pos[i] if i < len(pos) else None,
-                load[i] if i < len(load) else None
-            ]
-            writer.writerow(row)
-    rospy.loginfo("Data saved to data.csv")
-
-# Listener setup and shutdown handling
 def listener():
-    rospy.init_node('data_collection', anonymous=True)
+    rospy.init_node('data_collection_node', anonymous=True)
     rospy.Subscriber("/niryo_robot_hardware_interface/hardware_status", HardwareStatus, callback_HS)
-    rospy.Subscriber("/joint_states", JointState, callback_JS)
-    
-    # Register the save_to_csv function to be called upon node shutdown
-    rospy.on_shutdown(save_to_csv)
-    
-    rospy.spin()  # Keep the node running until interrupted
+    rospy.spin()
+
 
 if __name__ == '__main__':
-    listener()
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        pass
+    finally:
+        csv_file.close()
