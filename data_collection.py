7b90e4b008df78f6bafe4212a2af66eb299d50c6
Hari Ganesan <hariganation@gmail.com>
2023-11-06 15:34:20 +0530

    removed joint states

--- data_collection.py
+++ data_collection.py
@@ -1,42 +1,37 @@
 #!/usr/bin/env python3
 
-import csv
 import rospy
+import csv
 from niryo_robot_msgs.msg import HardwareStatus
-from sensor_msgs.msg import JointState
 
-# Open the CSV file for writing
-csv_file = open('temperature_voltage_data.csv', 'w', newline='')
+# Setup CSV writer
+csv_file = open('/home/smarton/capstone/temperature_voltage_data.csv', 'w', newline='')
 writer = csv.writer(csv_file)
-
-# Write the header row based on the expected message fields
-header = ["Seq", "Time Secs", "Time Nsecs", "Temperatures", "Voltages"]
-writer.writerow(header)
+writer.writerow(["Seq", "Time Secs", "Temperatures", "Voltages"])
 
 def callback_HS(data):
-    # Create a list of the data fields you want to write to the CSV
-    row = [
-        data.header.seq,
-        data.header.stamp.secs,
-        data.header.stamp.nsecs,
-        ';'.join(map(str, data.temperatures)),  # Convert each temperature to string and join
-        ';'.join(map(str, data.voltages)),      # Convert each voltage to string and join
-    ]
-    
-    # Write the data to the CSV and flush to ensure it's written to disk
-    writer.writerow(row)
-    csv_file.flush()
+    try:
+        rospy.loginfo("Received message on /niryo_robot_hardware_interface/hardware_status")
+        row = [
+            data.header.seq,
+            data.header.stamp.secs,
+            ';'.join(map(str, data.temperatures)),
+            ';'.join(map(str, data.voltages)),
+        ]
+        writer.writerow(row)
+        csv_file.flush()
+    except Exception as e:
+        rospy.logerr("Error in callback_HS: {}".format(e))
 
 def listener():
     rospy.init_node('data_collection_node', anonymous=True)
     rospy.Subscriber("/niryo_robot_hardware_interface/hardware_status", HardwareStatus, callback_HS)
     rospy.spin()
 
-
 if __name__ == '__main__':
     try:
         listener()
     except rospy.ROSInterruptException:
-        pass
+        rospy.loginfo("Shutting down data collection node")
     finally:
         csv_file.close()
