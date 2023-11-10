81a175925edfb508a9d7e741519c262d2699eabe
Hari Ganesan <hariganation@gmail.com>
2023-11-10 16:20:36 +0530

    Updated sender file

--- data_sender.py
+++ data_sender.py
@@ -12,37 +12,49 @@ device_connection_string = "HostName=niryoiot.azure-devices.net;DeviceId=NiryoNe
 device_client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
 device_client.connect()
 
+combined_data = {}
+data_lock = threading.Lock()
+last_update = 0
 
 def __callback_hs(data):
-    global combined_data
     try:
-        rospy.loginfo("Received message on /niryo_robot_hardware_interface/hardware_status")
-        x = {
-            "temperatures": data.temperatures,
-            "voltages": data.voltages,
-        }
-        device_client.send_message(str(x))
+        update_data("hardware_status", {"temperatures": data.temperatures, "voltages": data.voltages})
     except Exception as e:
         rospy.logerr("Error in callback: {}".format(e))
 
 def __callback_js(data):
     try:
-        rospy.loginfo("Received message on /joint_states")
-        x = {
-            "effort": data.effort, 
-            "position": data.position
-        }
-        device_client.send_message(str(x))
+        update_data("joint_states", {"effort": data.effort, "position": data.position})
     except Exception as e:
         rospy.logerr("Error in callback: {}".format(e))
+
+def update_data(key, value):
+    global combined_data, last_update_time
+    with data_lock:
+        combined_data[key] = value
+        last_update_time = time.time()
+
+def send_data():
+    global combined_data, last_update_time
+    send_interval = 0.5
+    while not rospy.is_shutdown():
+        time.sleep(send_interval)
+        with data_lock:
+            if time.time() - last_update_time < send_interval:
+                try:
+                    device_client.send_message(json.dumps(combined_data))
+                    rospy.loginfo("Sent data to Azure: {}".format(combined_data))
+                except Exception as e:
+                    rospy.logerr("Error sending data to Azure: {}".format(e))
 
 def listener():
     rospy.init_node('data_sender_node', anonymous=True)
     rospy.Subscriber("/niryo_robot_hardware_interface/hardware_status", HardwareStatus, __callback_hs)
     rospy.Subscriber("/joint_states", JointState, __callback_js)
-    rate = rospy.Rate(1)
-    while not rospy.is_shutdown():
-        rate.sleep()
+
+    send_thread = threading.Thread(target=send_data)
+    send_thread.start()
+
     rospy.spin()
 
 if __name__ == "__main__":
