49409277d8b30792f58bac4b5d8a3213407d682d
Hari Ganesan <hariganation@gmail.com>
2023-11-13 18:27:34 +0530

    Updated DT Controller

--- Unity_code/SimpleRobotController_step.cs
+++ Unity_code/SimpleRobotController_step.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
-namespace Unity.Robotics.UrdfImporter.Control //Smoother movement
+namespace Unity.Robotics.UrdfImporter.Control //Stepper movement
 {
-    public class SimpleRobotController : MonoBehaviour
+    public class SimpleRobotController_step : MonoBehaviour
     {
         private readonly string[] jointNames = {
             "shoulder_link", "arm_link", "elbow_link",
@@ -10,19 +10,15 @@ namespace Unity.Robotics.UrdfImporter.Control //Smoother movement
 
         private ArticulationBody[] jointArticulationBodies;
 
+        // Control parameters (customize these as needed)
         public float stiffness = 10000f;
         public float damping = 100f;
         public float forceLimit = 1000f;
-        private float[] startPositions;
-        private float[] targetPositions;
-        private float moveStartTime;
-        public float movementDuration = 2.0f;
 
         void Start()
         {
             jointArticulationBodies = new ArticulationBody[jointNames.Length];
 
-            // Find and store a reference to each joint's ArticulationBody
             for (int i = 0; i < jointNames.Length; i++)
             {
                 ArticulationBody joint = GameObject.Find(jointNames[i]).GetComponent<ArticulationBody>();
@@ -39,53 +35,30 @@ namespace Unity.Robotics.UrdfImporter.Control //Smoother movement
                 jointDrive.forceLimit = forceLimit;
                 joint.xDrive = jointDrive;
             }
-
-            startPositions = new float[jointNames.Length];
-
         }
 
-        public void SetTargetPositions(float[] targets)
+        public void MoveRobot(float[] targetPositions)
         {
-            if (targets.Length != jointArticulationBodies.Length)
+            if (targetPositions.Length != jointArticulationBodies.Length)
             {
                 Debug.LogError("SimpleRobotController: The targetPositions array length does not match the number of robot joints.");
                 return;
             }
 
-            moveStartTime = Time.time;
             for (int i = 0; i < jointArticulationBodies.Length; i++)
             {
-                startPositions[i] = jointArticulationBodies[i].xDrive.target;
+                var jointDrive = jointArticulationBodies[i].xDrive;
+                jointDrive.target = targetPositions[i];
+                jointArticulationBodies[i].xDrive = jointDrive;
             }
-
-            targetPositions = targets;
         }
 
         void Update()
         {
-            if (targetPositions != null)
-            {
-                float fraction = (Time.time - moveStartTime) / movementDuration;
-
-                if (fraction >= 1)
-                {
-                    fraction = 1;
-                    targetPositions = null;
-                }
-
-                for (int i = 0; i < jointArticulationBodies.Length; i++)
-                {
-                    float interpolatedPosition = Mathf.Lerp(startPositions[i], targetPositions[i], fraction);
-                    var jointDrive = jointArticulationBodies[i].xDrive;
-                    jointDrive.target = interpolatedPosition;
-                    jointArticulationBodies[i].xDrive = jointDrive;
-                }
-            }
-
             if (Input.GetKeyDown(KeyCode.M))
             {
-                float[] newTargetPositions = { 90f, 0f, 0f, 0f, 0f, 0f };
-                SetTargetPositions(newTargetPositions);
+                float[] targetJointPositions = { 90f, 0f, 0f, 0f, 0f, 0f };
+                MoveRobot(targetJointPositions);
             }
         }
     }
