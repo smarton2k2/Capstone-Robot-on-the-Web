e328892b54db996bfc311c473ce5f27f182f2382
Hari Ganesan <hariganation@gmail.com>
2023-11-14 22:30:06 +0530

    Added kernel messages for rpi

--- data_sender.py
+++ data_sender.py
@@ -7,6 +7,7 @@ from sensor_msgs.msg import JointState
 import time
 import json
 import threading
+import os
 
 device_connection_string = "HostName=niryoiot.azure-devices.net;DeviceId=NiryoNed2;SharedAccessKey=Ee0uclVHdESQI+NPmG8z5G3ZA1ET0dkpiZ5lbyOY4Oo=="
 device_client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
@@ -39,15 +40,24 @@ def update_data(key, value):
 def send_data():
     global combined_data, last_update_time
     send_interval = 0.75
+    alive_interval = 60
+    last_alive_time = time.time()
+
     while not rospy.is_shutdown():
         time.sleep(send_interval)
+        current_time = time.time()
         with data_lock:
-            if time.time() - last_update_time < send_interval:
+            if current_time - last_update_time < send_interval:
                 try:
                     device_client.send_message(json.dumps(combined_data))
                     rospy.loginfo("Sent data to Azure: {}".format(combined_data))
                 except Exception as e:
                     rospy.logerr("Error sending data to Azure: {}".format(e))
+
+            if current_time - last_alive_time >= alive_interval:
+                alive_message = "Alive message from Raspberry Pi at {}".format(time.ctime())
+                send_console_message(alive_message)
+                last_alive_time = current_time
 
 def listener():
     rospy.init_node('data_sender_node', anonymous=True)
