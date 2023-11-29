37174d7e0edff87e37e751bed68a0150e3b0ee78
Hari Ganesan <hariganation@gmail.com>
2023-11-29 13:09:03 +0530

    Updated recieve code and added send code for digital twin

--- Niryo/DT_recieve.py
+++ Niryo/DT_recieve.py
@@ -1,15 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from azure.eventhub import EventHubConsumerClient
 import threading
 import json
 import sys
 from azure.iot.device import IoTHubDeviceClient, Message
 
-EVENTHUB_COMPATIBLE_ENDPOINT = "sb://ihsuprodpnres017dednamespace.servicebus.windows.net/"
-EVENTHUB_COMPATIBLE_PATH = "iothub-ehub-niryoiot-25263541-af3f4ab300"
-IOTHUB_SAS_KEY = "JCqKnk2r6LzjU3QQyd74eGLHneplSDlHmAIoTHWhsic="
-SHARED_ACCESS_KEY_NAME = "iothubowner"
+#free
+# EVENTHUB_COMPATIBLE_ENDPOINT = "sb://ihsuprodpnres017dednamespace.servicebus.windows.net/"
+# EVENTHUB_COMPATIBLE_PATH = "iothub-ehub-niryoiot-25263541-af3f4ab300"
+# IOTHUB_SAS_KEY = "JCqKnk2r6LzjU3QQyd74eGLHneplSDlHmAIoTHWhsic="
+# SHARED_ACCESS_KEY_NAME = "iothubowner"
 
+#paid
+EVENTHUB_COMPATIBLE_ENDPOINT="sb://iothub-ns-niryo-send-25375228-cd4754aa95.servicebus.windows.net/"
+EVENTHUB_COMPATIBLE_PATH="niryo-sender"
+SHARED_ACCESS_KEY_NAME="iothubowner"
+IOTHUB_SAS_KEY="cmU6OdUFbrmukNxewPAisOR7yZ+nhO1OAAIoTDI2DTY="
+
+#free
 device_connection_string = "HostName=niryoiot.azure-devices.net;DeviceId=NiryoNed2;SharedAccessKey=Ee0uclVHdESQI+NPmG8z5G3ZA1ET0dkpiZ5lbyOY4Oo=="
 device_client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
 device_client.connect()
@@ -22,9 +30,9 @@ app = Flask(__name__)
 def get_data():
     if latest_data:
         return latest_data
-    return jsonify({"error": "No data received yet."})
+    return jsonify({"error": "No data received yet."}), 500
 
-@app.route('/your-endpoint', methods=['POST'])
+@app.route('/recieve_data', methods=['POST'])
 def receive_data_from_unity():
     try:
         data = request.json
@@ -39,7 +47,6 @@ def azure_data_receiver(partition_context, data_set):
     for data in data_set:
         latest_data = data.body_as_str()
         latest_data = json.loads(latest_data)
-        # print(latest_data["position"])
     partition_context.update_checkpoint()
 
 def azure_receiver():
@@ -57,4 +64,4 @@ def azure_receiver():
 if __name__ == '__main__':
     azure_thread = threading.Thread(target=azure_receiver)
     azure_thread.start()
-    app.run(port=5000)
+    app.run(port=5000,debug=False)
